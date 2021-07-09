50826c932f1e422a54ee818eca79635939de5e5d
kuber verma <70593413+kverma007@users.noreply.github.com>
2021-07-09 11:25:18 +0530

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,14 @@
+//Day - 2 Javs Script Essentials Program 
+
+//A program to print 5 times "*" horizontally in a single row
+
+for (i = 0; i <= 0; i++){
+    let v = "";
+
+    for (j = 0; j <= 4; j++){
+        v=v+" *";
+    }
+
+console.log(v);
+}
+
